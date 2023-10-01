ea2a4a6dc33121de1fa742431d4eca03b370dd9e
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-10-02 00:52:09 +0300

    Create arch-gnome-to-kde.sh

---
+++ arch-gnome-to-kde.sh
@@ -0,0 +1,24 @@
+yay -S plasma-wayland-session plasma-desktop maliit-keyboard konsole
+yay -Rns gnome-autoar gnome-backgrounds gnome-backgrounds-macos gnome-bluetooth gnome-color-manager
+gnome-backgrounds 44.0-1
+gnome-backgrounds-macos 43.0.0-1
+gnome-bluetooth-3.0 42.6-1
+gnome-color-manager 3.36.0+r42+g90481514-1
+gnome-control-center 44.4-1
+gnome-desktop 1:44.0-1
+gnome-desktop-4 1:44.0-1
+gnome-desktop-common 1:44.0-1
+gnome-disk-utility 45.0-1
+gnome-keyring 1:42.1-3
+gnome-online-accounts 3.48.0-2
+gnome-session 44.0-2
+gnome-settings-daemon 44.1-1
+gnome-shell 1:44.5-1
+gnome-software-git 45.0+r10+ga8cc166ae-1
+gnome-software-packagekit-plugin-git 45.0+r10+ga8cc166ae-1
+gnome-system-monitor 44.0-1
+gnome-terminal 3.48.2-1
+gnome-tweaks 42beta+r14+g2d9d76c-3
+libgnomekbd 1:3.28.1-1
+polkit-gnome 0.105-10
+xdg-desktop-portal-gnome 44.2-1
