76c7c1629c185cc7c0252beb357bac6e11ae6623
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-10-02 01:07:26 +0300

    Update arch-gnome-to-kde.sh

--- arch-gnome-to-kde.sh
+++ arch-gnome-to-kde.sh
@@ -1,24 +1,15 @@
-yay -S plasma-wayland-session plasma-desktop maliit-keyboard konsole
-yay -Rns gnome-autoar gnome-backgrounds gnome-backgrounds-macos gnome-bluetooth gnome-color-manager
-gnome-backgrounds 44.0-1
-gnome-backgrounds-macos 43.0.0-1
-gnome-bluetooth-3.0 42.6-1
-gnome-color-manager 3.36.0+r42+g90481514-1
-gnome-control-center 44.4-1
-gnome-desktop 1:44.0-1
-gnome-desktop-4 1:44.0-1
-gnome-desktop-common 1:44.0-1
-gnome-disk-utility 45.0-1
-gnome-keyring 1:42.1-3
-gnome-online-accounts 3.48.0-2
-gnome-session 44.0-2
-gnome-settings-daemon 44.1-1
-gnome-shell 1:44.5-1
-gnome-software-git 45.0+r10+ga8cc166ae-1
-gnome-software-packagekit-plugin-git 45.0+r10+ga8cc166ae-1
-gnome-system-monitor 44.0-1
-gnome-terminal 3.48.2-1
-gnome-tweaks 42beta+r14+g2d9d76c-3
-libgnomekbd 1:3.28.1-1
-polkit-gnome 0.105-10
-xdg-desktop-portal-gnome 44.2-1
+#!/bin/sh
+# Removing Gnome only Configs
+sudo sed -i '/export QT_QPA_PLATFORMTHEME=qt5ct/d' /etc/environment
+sudo systemctl disable gdm
+
+# Removing Gnome and GTK apps
+yay -Rns --noconfirm --needed gnome-autoar gnome-backgrounds gnome-backgrounds-macos gnome-bluetooth gnome-color-manager gnome-control-center gnome-desktop gnome-disk-utility gnome-keyring gnome-online-accounts gnome-session gnome-settings-daemon gnome-shell  gnome-software-git gnome-software-packagekit-plugin-git gnome-system-monitor gnome-terminal gnome-tweaks polkit-gnome xdg-desktop-portal-gnome adwaita-qt5-git adwaita-qt6-git qt5ct qt6ct adw-gtk-theme qt5ct qt6ct nautilus-image-converter nautilus nautilus-checksums nautilus-code
+flatpak remove -y org.gnome.gitlab.cheywood.Iotas org.gnome.Loupe org.gnome.Calculator org.gnome.Calendar org.gnome.Characters org.gnome.SimpleScan org.gnome.Evince org.gnome.font-viewer org.gnome.Snapshot org.gnome.Epiphany org.gtk.Gtk3.theme.adw-gtk3 org.gtk.Gtk3.theme.adw-gtk3-dark 
+
+# Installing KDE and QT apps
+yay -Syu --noconfirm --needed plasma-wayland-session plasma-desktop maliit-keyboard konsole dolphin discover packagekit-qt5 plasma-systemmonitor partitionmanager plasma-disks sddm-git
+flatpak install -y org.kde.kcalc org.qownnotes.QOwnNotes org.kde.gwenview org.kde.kamoso org.kde.okular org.kde.angelfish org.kde.haruna
+
+# Kde Configs
+sudo systemctl enable sddm
