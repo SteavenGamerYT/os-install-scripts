576c252872535104b1a62f795dd3517f8a7eb578
SteavenGamerYT <omarhanykasban706@gmail.com>
2024-06-05 05:49:49 +0300

    a

--- arch/arch.sh
+++ arch/arch.sh
@@ -22,7 +22,10 @@ sudo sed -i 's/\(HOOKS=.*filesystems\) \(fsck.*\)/\1 resume \2/' /etc/mkinitcpio
 sudo mkinitcpio -P
 
 # Packages
-yay -Syu --noconfirm --needed pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack gnome-shell gnome-session gnome-control-center gdm gnome-tweaks xdg-desktop-portal xdg-desktop-portal-gnome gnome-keyring gnome-terminal adwaita-qt5-git adwaita-qt6-git qt5ct qt6ct adw-gtk-theme flatpak snapd cups foomatic-db-engine foomatic-db foomatic-db-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds gutenprint foomatic-db-gutenprint-ppds hplip hpuld hplip-plugin epson-inkjet-printer-escpr2 epson-inkjet-printer-escpr2 epson-inkjet-printer-escpr distrobox podman yt-dlp zsh zsh-syntax-highlighting autojump zsh-autosuggestions neofetch xclip lolcat git trash-cli nextcloud-client openssh hplip ufw nano iio-sensor-proxy dconf ttf-roboto ttf-roboto-mono ttf-ubuntu-font-family noto-fonts-cjk noto-fonts-emoji noto-fonts xdg-user-dirs nautilus power-profiles-daemon starship shell-color-scripts ttf-meslo-nerd-font-powerlevel10k ttf-croscore ttf-fira-code ttf-firacode-nerd ttf-hack ttf-meslo ttf-meslo-nerd terminus-font ttf-ubuntu-mono-nerd gstreamer gst-libav gst-plugins-bad  gst-plugins-base gst-plugins-good  gst-plugins-ugly gst-plugins-ugly libde265 gst-plugin-openh264 nautilus-checksums nautilus-code webcord ttf-roboto-mono-nerd p7zip file-roller unrar unzip zip unace lrzip gnome-disk-utility gnome-system-monitor gparted dosfstools jfsutils f2fs-tools btrfs-progs exfatprogs ntfs-3g reiserfsprogs udftools xfsprogs nilfs-utils gpart mtools aic94xx-firmware linux-firmware-qlogic wd719x-firmware upd72020x-fw epson-printer-utility qt4-bin packagekit gnome-software-packagekit-plugin fwupd vlc-git touchegg kvantum-theme-libadwaita-git kvantum ffmpegthumbnailer gvfs-smb xf86-video-intel vulkan-intel lib32-vulkan-intel intel-media-driver wine-gecko wine-mono winetricks wine-staging goverlay mangohud gamemode lib32-gamemode qemu-desktop libvirt edk2-ovmf virt-manager dnsmasq usbutils speech-dispatcher wget jre17-openjdk jre8-openjdk bleachbit nautilus-image-converter popcorntime-bin hunspell-ar python-pyqt5 baobab vim neovim system-config-printer
+yay -Syu --noconfirm --needed $(cat core-packages.txt)
+yay -Syu --noconfirm --needed $(cat printer-packages.txt)
+yay -Syu --noconfirm --needed $(cat personal-packages.txt)
+yay -Syu --noconfirm --needed $(cat i3-packages.txt)
 yay -Syu --noconfirm --needed ttf-ms-win11-auto
 
 # Flathub
@@ -31,11 +34,9 @@ sudo flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-re
 sudo flatpak remote-add --if-not-exists appcenter https://flatpak.elementary.io/repo.flatpakrepo
 
 # Systemctl
-sudo systemctl enable gdm
+sudo systemctl enable sddm
 sudo systemctl enable --now cups
 sudo systemctl enable --now cups-browsed
-sudo systemctl enable --now ecbd.service
-sudo systemctl enable --now touchegg
 
 # Initialize virtualization
 sudo sed -i 's/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/g' /etc/libvirt/libvirtd.conf
@@ -51,23 +52,19 @@ sudo usermod -a -G uucp,lp $USER
 
 # Install things I need, top is uncategorized
 # Install the packages using flatpak
-flatpak install -y flathub $(cat flathub-packages.txt)
+flatpak install -y flathub $(cat ../flathub-packages.txt)
 
 # Games
 # Install the packages using flatpak
-flatpak install -y flathub $(cat flathub-games.txt)
+flatpak install -y flathub $(cat ../flathub-games.txt)
 
 # Install Beta version of GIMP. It performs better than the stable one, plus better Wayland support.
 # Install the packages using flatpak
-flatpak install -y flathub-beta $(cat flathub-beta-packages.txt)
+flatpak install -y flathub-beta $(cat ../flathub-beta-packages.txt)
 
 # appcenter (Elementery os)
 # Install the packages using flatpak
-flatpak install -y appcenter $(cat appcenter-packages.txt)
-
-# Install things I need using dnf, top is uncategorized
-# Install the packages using dnf
-sudo dnf install -y $(cat fedora-packages.txt)
+flatpak install -y appcenter $(cat ../appcenter-packages.txt)
 
 # Some Configs
 # Locales
@@ -106,38 +103,14 @@ sudo ufw allow 27036/udp comment "steam client discovery"
 sudo ufw reload
 sudo ufw enable
 
-# gnome
-sudo mkdir -p /etc/dconf/profile
-sudo mkdir -p /etc/dconf/db/local.d
-sudo mkdir -p /etc/dconf/db/locks
-echo "user-db:user" | sudo tee -a /etc/dconf/profile/user
-echo "system-db:local" | sudo tee -a /etc/dconf/profile/user
-echo "[org/gnome/mutter]" | sudo tee -a /etc/dconf/db/local.d/00-hidpi
-echo "experimental-features=['scale-monitor-framebuffer']" | sudo tee -a /etc/dconf/db/local.d/00-hidpi
-echo "/org/gnome/mutter/experimental-features" | sudo tee -a /etc/dconf/db/locks/hidpi
-sudo dconf update
-gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-echo "--enable-features=WaylandWindowDecorations" | tee -a ~/.config/electron25-flags.conf
-echo "--ozone-platform-hint=auto" | tee -a ~/.config/electron25-flags.conf
-gsettings set org.gnome.shell.keybindings show-screenshot-ui "['<Shift><Super>S','Print']"
-
-# Omar-Share
-echo "# Omar-share" | sudo tee -a /etc/fstab
-echo "//192.168.1.31/omar-share2 /media/omar-share2 cifs username=omarhanykasban,password=omargamer1234,rw,nounix,iocharset=utf8,file_mode=0777,dir_mode=0777,nofail 0 0" | sudo tee -a /etc/fstab
-echo "//192.168.1.31/omar-share /media/omar-share cifs username=omarhanykasban,password=omargamer1234,rw,nounix,iocharset=utf8,file_mode=0777,dir_mode=0777,nofail 0 0" | sudo tee -a /etc/fstab
-
 # Enable Kicksecure CPU mitigations + Kicksecure's CPU distrust script + Kicksecure's IOMMU patch (limits DMA)
 find /boot/loader/entries -type f ! -name "*fallback*" -exec grep -q 'options' {} \; -exec  sudo sed -i "s/options.*/& spectre_v2=on spec_store_bypass_disable=on l1tf=full,force mds=full,nosmt tsx=off tsx_async_abort=full,nosmt kvm.nx_huge_pages=force nosmt=force l1d_flush=on mmio_stale_data=full,nosmt random.trust_cpu=off intel_iommu=on amd_iommu=on efi=disable_early_pci_dma iommu.passthrough=0 iommu.strict=1/" {} +
 
-# GrapheneOS's ssh limits
-# caps the system usage of sshd
-sudo mkdir -p /etc/systemd/system/sshd.service.d
-sudo curl https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/systemd/system/sshd.service.d/local.conf -o /etc/systemd/system/sshd.service.d/local.conf
-
-# NTS instead of NTP
-# NTS is a more secured version of NTP
-sudo curl https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf -o /etc/chrony.conf
-
+# Steaven Settings
+cd ~
+git clone https://github.com/SteavenGamerYT/SteavenSettings
+cd SteavenSettings
+./install.sh
+cd ~
 
 echo "The configuration is now complete."
---
+++ arch/nvidia-beta.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yay -S aur/lib32-nvidia-utils-beta aur/lib32-opencl-nvidia-beta aur/nvidia-beta-dkms aur/nvidia-settings-beta aur/nvidia-utils-beta aur/opencl-nvidia-beta
